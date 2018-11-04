4f05ac8ff9849d0efbd7a1f6a72d9ed6b5180818
Anny Guttierrez <agutilop@everis.com>
2018-11-04 17:31:09 -0500

    Updates

---
+++ precache-manifest.13faa71082d2838ebe66965f462c1043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/annygutierrez/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "817cb9f6273b2bed7a8e",
+    "url": "/annygutierrez/static/js/runtime~main.817cb9f6.js"
+  },
+  {
+    "revision": "a2424e05c45cf9b01286",
+    "url": "/annygutierrez/static/js/main.a2424e05.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/annygutierrez/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "a2424e05c45cf9b01286",
+    "url": "/annygutierrez/static/css/main.9e38049f.chunk.css"
+  },
+  {
+    "revision": "f7050127d94e7f8ed07d52dfbdc689c6",
+    "url": "/annygutierrez/index.html"
+  }
+];
