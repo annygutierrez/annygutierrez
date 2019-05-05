393682bd473f8b9ea74e345c8eb3d309a6a249bd
annygutierrez <annygutierrezlopez@gmail.com>
2019-05-04 21:00:00 -0500

    Add functionality to the side drawer
    - Add functionality to close and open the side drawer.

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,9 +4,12 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
-  // ...
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
+    <div className={attachedClasses.join(' ')}>
       <div className={classes.Logo}>
         <Logo />
       </div>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
@@ -8,10 +9,10 @@ const toolbar = (props) => (
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <div>NADA</div>
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
     </header>
 );
 
