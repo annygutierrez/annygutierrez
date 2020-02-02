b075d0332d5d3f2f713261d6938fe9f0c6d56c9d
annygutierrez16 <agutilop@everis.com>
2020-02-01 19:38:33 -0500

    Updates

---
+++ precache-manifest.77691cd128d1a3e729eb1e69696ea35d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacc2638ae831c66e37c6d7124e862a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5486fb1b626c5f429db",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e7279ada14942df002de",
+    "url": "/static/css/main.5f7b0f2e.chunk.css"
+  },
+  {
+    "revision": "e5486fb1b626c5f429db",
+    "url": "/static/js/2.d01ca414.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d01ca414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7279ada14942df002de",
+    "url": "/static/js/main.6fdb5476.chunk.js"
+  },
+  {
+    "revision": "f35392193b7d8210b557",
+    "url": "/static/js/runtime-main.c05ffe79.js"
+  },
+  {
+    "revision": "3b70819e5719108d85ab17ec22307bb1",
+    "url": "/static/media/arrow.3b70819e.svg"
+  },
+  {
+    "revision": "ff954aaf5db0a32b95b04a3345148a67",
+    "url": "/static/media/kaytrust.ff954aaf.svg"
+  },
+  {
+    "revision": "0360a849db9f8d7fb093bc4e7cbf1920",
+    "url": "/static/media/kaytrustweb.0360a849.svg"
+  },
+  {
+    "revision": "ba0740a990000316a9d893b9697e1703",
+    "url": "/static/media/label-icon.ba0740a9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "642b85e2d04133507b4b5ed14b02f466",
+    "url": "/static/media/menu-icon.642b85e2.svg"
+  },
+  {
+    "revision": "55814f916acf8540dfe088de649f2ece",
+    "url": "/static/media/user.55814f91.svg"
+  }
+]);
