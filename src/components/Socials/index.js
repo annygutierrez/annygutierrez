2a5df9998b42982cbe941aacfe853ae736232fc8
Anny Fiorella <annygutierrezlopez@gmail.com>
2022-11-08 14:14:44 -0500

    chore(Components): Comment instagram link

--- src/components/Socials/index.js
+++ src/components/Socials/index.js
@@ -8,7 +8,7 @@ import './styles.css';
 
 const SocialsLinks = {
     twitter: 'https://twitter.com/annygutierrezl',
-    insta: 'https://www.instagram.com/annyglop16/',
+    // insta: 'https://www.instagram.com/annyglop16/',
     github: 'https://github.com/annygutierrez',
     linkedin: 'https://www.linkedin.com/in/annygutierrez/'
 }
@@ -18,7 +18,7 @@ const Socials = () => {
     return (
         <div className="Socials">
             <SocialItem link={SocialsLinks.twitter} icon={Twitter} alt='twitter' />
-            <SocialItem link={SocialsLinks.insta} icon={Insta} alt='insta' />
+            {/* <SocialItem link={SocialsLinks.insta} icon={Insta} alt='insta' /> */}
             <SocialItem link={SocialsLinks.github} icon={Github} alt='git' />
             <SocialItem link={SocialsLinks.linkedin} icon={Linkedin} alt='linkedin' />
         </div>
